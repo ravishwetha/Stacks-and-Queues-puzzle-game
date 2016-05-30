ff6a3200c22fba12ac7150a1baec04b14f670618
ravishwetha <shwetha.ravi3@gmail.com>
2016-05-30 23:43:20 +0800

    Update main.cpp

--- Stacks and Queues/main.cpp
+++ Stacks and Queues/main.cpp
@@ -1,8 +1,9 @@
-
 #include <SFML/Graphics.hpp>
 #include <math.h>
 #include <iostream>
 #include <string>
+#include <time.h>
+using namespace std;
 
 /// Globals
 sf::RenderWindow* window;
@@ -22,6 +23,8 @@ private:
     
 public:
     bool isActive;
+    bool isMoving;
+    bool isOnScreen;
     
     Ball(float x, float y, float vx, float vy, float radius, sf::Color color, sf::String label) {
         this->x = x;
@@ -34,21 +37,34 @@ public:
         
         dia = radius*2;
         isActive = true;
+        isMoving = false;
     }
     
-    void update() {
-        if(!isActive) return;
+    bool checkOnScreen() {
+        if (x < -(radius) || y < -(radius) || x > RES_X + x || y > RES_Y + y) {
+            return false;
+        }
+        return true;
+    }
+    
+    void update(bool status) {
+        isOnScreen = checkOnScreen();
+        
+        isMoving = status;
+        if(!isMoving || !isActive) return;
         x -= vx;
         if (x < -(dia) ||
             y < -(dia)) isActive = false;
     }
     
     void draw() {
+        if(!isOnScreen || !isActive) return;
+        
         sf::CircleShape ballCircle;
         ballCircle.setFillColor(color);
         ballCircle.setRadius(radius);
         ballCircle.setPosition(x, y);
-        //shape.setOrigin(radius, radius);
+        ballCircle.setOrigin(radius, radius);
         
         //place label on ball
         // Declare and load a font
@@ -59,29 +75,30 @@ public:
         ballText.setCharacterSize(dia);
         ballText.setStyle(sf::Text::Bold);
         ballText.setColor(sf::Color::White);
-        ballText.setPosition(x+dia/5, y-dia/10);
-        
+        //ballText.setOrigin(x+radius, y+radius);
+        ballText.setPosition(x-(radius/1.50), y-(radius/0.75));
         
         window->draw(ballCircle);
         window->draw(ballText);
     }
 };
 
-Ball* lvl1ball1;
-Ball* lvl1ball3;
-Ball* lvl1ball4;
-
 class Level {
 private:
-    std::vector<float> ballsPositionX { RES_X, RES_X };
-    std::vector<float> ballsPositionY { RES_Y2, RES_Y2 };
-    std::vector<float> ballsVX { RES_X/1000, RES_X/1000 }; //respective vx of balls in a level by level number
-    std::vector<float> ballsPeriod { 2.0, 4.9 }; //balls appear on screen every x seconds
-    std::vector<float> ballsRadii { 10, 10 };
+    std::vector<float> ballsPositionX { RES_X, RES_X , RES_X};
+    std::vector<float> ballsPositionY { RES_Y2, RES_Y2, RES_Y };
+    std::vector<float> ballsPeriod { 2.0, 2.0, 2.0 }; //balls appear on screen every x seconds
+    std::vector<float> ballsVX { 1.0, 1.0, 1.0 }; //respective vx of balls in a level by level number
+    std::vector<float> ballsRadii { 10, 10, 10 };
     
 public:
     int num;
     bool isActive;
+    bool domino; //when the level starts, the movement of the first ball triggers the movement of the rest at constant intervals
+    
+    //objects in level
+    clock_t ballClock;
+    std::vector<Ball> balls;
     
     Level(int num) {
         this->num = num;
@@ -108,17 +125,78 @@ public:
         return ballsRadii.at(num);
     }
     
+    void startBallClock() {
+        ballClock = clock();
+    }
+    
+    float getInterval() {
+        clock_t interval;
+        interval = clock() - ballClock;
+        return (((float)interval)/CLOCKS_PER_SEC);
+    }
+    
+    void updateLevel() {
+        
+        for( int i = 0; i < balls.size(); i = i + 1 ) {
+            //cout << "Ball " << i << ":";
+            
+            //ball went through exit tube
+            if(!balls.at(i).isActive) {
+                continue;
+            }
+            
+            //before ball starts moving
+            if(!(balls.at(i).isMoving)) {
+                if(i == 0) {
+                    //cout << "Ball 0 updated and moving" << ".\n";
+                    balls.at(i).update(true);
+                    startBallClock();
+                }
+                else {
+                    cout << getInterval();
+                    if(getInterval() >= ballsPeriod.at(i-1)) {
+                        //cout << " Ball " << i << " moved after right interval" << ".\n";
+                        balls.at(i).update(true);
+                        startBallClock();
+                    }
+                }
+            }
+            //after ball is moving
+            else {
+                //cout << "Ball " << i << "is already moving" << ".\n";
+                balls.at(i).update(true);
+            }
+        }
+    }
+    
+    //need to make it better than a white rectangle
     void drawInTube() {
-        float width = ballsRadii.at(num)*4;
+        float width = (ballsRadii.at(num)*2)*3; //can hold three balls at a time
         float height = ballsRadii.at(num)*2;
         sf::RectangleShape inTube;
         inTube.setFillColor(sf::Color::White);
         inTube.setSize(sf::Vector2f(width, height));
-        inTube.setOrigin(ballsPositionX.at(num) - width, ballsPositionY.at(num) + (height/2));
-        inTube.setPosition(ballsPositionX.at(num), ballsPositionY.at(num));
+        inTube.setPosition(RES_X-width/2.0, RES_Y2);
+        inTube.setOrigin(width/2.0, height/2.0);
         window->draw(inTube);
     }
-
+    
+    void drawOutTube() {
+        //TO-DO
+    }
+    
+    void drawLevel() {
+        drawInTube();
+        drawOutTube();
+        for( int i = 0; i < balls.size(); i = i + 1 ) {
+            balls.at(i).draw();
+        }
+    }
+    
+    //once all balls are off screen
+    void clearLevel() {
+        
+    }
 };
 
 Level* level1;
@@ -129,19 +207,31 @@ void initialiseLevel1() {
     level1 = new Level(1);
     levels.push_back(*level1);
     
+    //get level info
     float x = level1->getBallPositionX();
     float y = level1->getBallPositionY();
     float vx1 = level1->getBallVX();
+    float vy1 = vx1;
     float period = level1->getBallPeriod();
-    float xPixInterval = vx1*12*period;
     float radius = level1->getBallRadius();
     
-    lvl1ball1 = new Ball(x, y, vx1, 0.5, radius, sf::Color::Red, "1");
-    lvl1ball3 = new Ball(x + xPixInterval, y, vx1, 0.5, radius, sf::Color::Red, "3");
-    lvl1ball4 = new Ball(x + xPixInterval*2, y, vx1, 0.5, radius, sf::Color::Red, "4");
+    //create level objects accordingly
+    Ball lvl1ball1 = Ball(x - (radius*5), y, vx1, vy1, radius, sf::Color::Red, "1");
+    Ball lvl1ball3 = Ball(x - (radius*3), y, vx1, vy1, radius, sf::Color::Red, "3");
+    Ball lvl1ball4 = Ball(x - (radius), y, vx1, vy1, radius, sf::Color::Red, "4");
+    
+    //store objects in level
+    level1->balls.push_back(lvl1ball1);
+    level1->balls.push_back(lvl1ball3);
+    level1->balls.push_back(lvl1ball4);
+}
+
+void initialiseMainMenu() {
+    //for a start, it says something like 'press enter to play'
 }
 
 void initialiseGame() {
+    initialiseMainMenu();
     initialiseLevel1();
 }
 
@@ -149,9 +239,7 @@ void updateGame() {
     
     //level1
     if(level1->isActive) {
-       lvl1ball1->update();
-       lvl1ball3->update();
-       lvl1ball4->update();
+        level1->updateLevel();
     }
 }
 
@@ -159,15 +247,13 @@ void drawGameFrame() {
     
     //level1
     if(level1->isActive) {
-        level1->drawInTube();
-        lvl1ball1->draw();
-        lvl1ball3->draw();
-        lvl1ball4->draw();
+        level1->drawLevel();
     }
 }
 
 void processEvent(sf::Event& event) {
     switch(event.type) {
+            
         case sf::Event::Closed: {
             window->close();
             break;
@@ -182,6 +268,7 @@ int main() {
     sf::Clock clock;
     float frameTime = 1/60.0f;
     float dTime = 0;
+    
     
     initialiseGame();
     
@@ -209,6 +296,8 @@ int main() {
         drawGameFrame();
         window->display();
     }
+    
+    //read somewhere that it was good c++ practice to delete all objects once they're out of scope
     
     delete window;
     return 0;
