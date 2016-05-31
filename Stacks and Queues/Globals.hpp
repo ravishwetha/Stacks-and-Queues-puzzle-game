8d0f533510ca971ab02fec6154da64c4fc8c6739
ravishwetha <shwetha.ravi3@gmail.com>
2016-05-31 19:28:24 +0800

    Add files via upload

---
+++ Stacks and Queues/Globals.hpp
@@ -0,0 +1,19 @@
+//Stacks and Queues
+//Copyright © 2016 Team SQ. All rights reserved.
+
+//Globals.hpp
+
+#ifndef Globals_hpp
+#define Globals_hpp
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+
+extern sf::RenderWindow* window;
+extern float RES_X;
+extern float RES_Y;
+extern float RES_Y2;
+
+#endif /* Globals_hpp */
