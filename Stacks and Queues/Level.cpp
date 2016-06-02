3902fd7d9d174d28e4c9bbfbaa62262ab62d405e
ravishwetha <shwetha.ravi3@gmail.com>
2016-06-02 14:13:45 +0800

    Draw outTube, added border and color for tubes
    I found out why outline wasn't showing up, if thickness is not specified, the default is 0 pixels. I also added colours to the tubes. Do you think the balls should be hidden while in the tube or should they visible to give the player some advantage? Or it could be a feature where it's visible only when the player's mouse is hovering over it.

--- Stacks and Queues/Level.cpp
+++ Stacks and Queues/Level.cpp
@@ -47,8 +47,9 @@ float Level::getInterval() {
 }
     
 void Level::updateLevel() {
-        
-    for( int i = 0; i < balls.size(); i = i + 1 ) {
+    
+    //UPDATE BALLS//
+    for( int i=0; i<balls.size(); i = i + 1 ) {
         //cout << "Ball " << i << ":";
             
         //ball went through exit tube
@@ -77,6 +78,11 @@ void Level::updateLevel() {
             //cout << "Ball " << i << "is already moving" << ".\n";
             balls.at(i).update(true);
         }
+        
+        //UPDATE STACKS//
+        for(int i=0; i<stacks.size(); i++) {
+            stacks.at(i).update();
+        }
     }
 }
 
@@ -84,26 +90,71 @@ void Level::updateLevel() {
 void Level::drawInTube() {
     float width = (ballsRadii.at(num)*2)*3; //can hold three balls at a time
     float height = ballsRadii.at(num)*2;
+    
+    sf::Color inner = sf::Color::Color(128, 128, 128); //currently gray 128
+    sf::Color outer = sf::Color::Color(192, 192, 192); //currently silver 192
+    
+    //inTube base rectangle
     sf::RectangleShape inTube;
-    inTube.setFillColor(sf::Color::White);
+    inTube.setFillColor(outer);
+    inTube.setOutlineColor(sf::Color::Black);
+    inTube.setOutlineThickness((height/5.0));
     inTube.setSize(sf::Vector2f(width, height));
-    inTube.setPosition(RES_X-width/2.0, RES_Y2);
     inTube.setOrigin(width/2.0, height/2.0);
+    inTube.setPosition(RES_X-width/2.0, RES_Y2);
     window->draw(inTube);
+    
+    sf::CircleShape leftEnd;
+    leftEnd.setFillColor(inner);
+    leftEnd.setOutlineColor(sf::Color::Black);
+    leftEnd.setOutlineThickness((height/5.0)); //Thickness can be negative so that the outline expands from the border towards center instead of increasing the shape's size.
+    leftEnd.setRadius(height/2);
+    leftEnd.setOrigin(width/2.0, height/2.0);
+    leftEnd.setPosition(RES_X - width*(2.0/3.0), RES_Y2);
+    window->draw(leftEnd);
 }
 
 void Level::drawOutTube() {
-    //TODO: Draw similiar tube on opposite end of screen
+    float width = (ballsRadii.at(num)*2)*3; //can hold three balls at a time
+    float height = ballsRadii.at(num)*2;
+    
+    sf::Color inner = sf::Color::Color(128, 128, 128); //currently gray 128
+    sf::Color outer = sf::Color::Color(192, 192, 192); //currently silver 192
+    
+    //inTube base rectangle
+    sf::RectangleShape outTube;
+    outTube.setFillColor(outer);
+    outTube.setOutlineColor(sf::Color::Black);
+    outTube.setOutlineThickness((height/5.0));
+    outTube.setSize(sf::Vector2f(width, height));
+    outTube.setOrigin(width/2.0, height/2.0);
+    outTube.setPosition(width/2.0, RES_Y2);
+    window->draw(outTube);
+    
+    sf::CircleShape rightEnd;
+    rightEnd.setFillColor(inner);
+    rightEnd.setOutlineColor(sf::Color::Black);
+    rightEnd.setOutlineThickness((height/5.0)); //Thickness can be negative so that the outline expands from the border towards center instead of increasing the shape's size.
+    rightEnd.setRadius(height/2);
+    rightEnd.setOrigin(width/2.0, height/2.0);
+    rightEnd.setPosition(width*(4.0/3.0), RES_Y2);
+    window->draw(rightEnd);
 }
     
 void Level::drawLevel() {
     drawInTube();
     drawOutTube();
-    for( int i = 0; i < balls.size(); i = i + 1 ) {
+    
+    //balls drawn after tubes makes tubes transparent
+    for( int i = 0; i < balls.size(); i++ ) {
         balls.at(i).draw();
+    }
+    //stacks drawn after balls make stacks obscure
+    for(int i=0; i<stacks.size(); i++) {
+        stacks.at(i).draw();
     }
 }
 
 void clearLevel() {
-        //TODO: clear all level items when off screen
+        //TODO: clear all level items when level ends
 }
