1ec6fac7276f511702333f1060319a80d2b5108e
ravishwetha <shwetha.ravi3@gmail.com>
2016-06-05 21:27:31 +0800

    sf::Rect<int> to sf::Rect<float> and backup main menu

--- Stacks and Queues/MainMenu.h
+++ Stacks and Queues/MainMenu.h
@@ -1,26 +1,37 @@
+//Stacks and Queues
+//Copyright © 2016 TeamSQ. All rights reserved.
+
+//MainMenu.h
+
+#ifndef MainMenu_h
+#define MainMenu_h
+
 #pragma once
 
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <list>
-	
+
+#include "Globals.hpp"
+
 class MainMenu
 {
-
 public:
-	enum MenuResult{Nothing, Exit, Play};
-
-	struct MenuItem
-	{
-	public:
-		sf::Rect<int> rect;
-		MenuResult action;
-	};
-
-	MenuResult Show(sf::RenderWindow& window);
-
+    enum MenuResult{Nothing, Backup, Exit, Play};
+    
+    struct MenuItem
+    {
+    public:
+        sf::Rect<float> rect;
+        MenuResult action;
+    };
+    
+    MenuResult Show(sf::RenderWindow& window);
+    
 private:
-	MenuResult GetMenuResponse(sf::RenderWindow& window);
-	MenuResult HandleClick(int x, int y);
-	std::list<MenuItem> menuItems;
+    MenuResult GetMenuResponse(sf::RenderWindow& window);
+    MenuResult HandleClick(int x, int y);
+    std::list<MenuItem> menuItems;
 };
+
+#endif /* MainMenu_h */
