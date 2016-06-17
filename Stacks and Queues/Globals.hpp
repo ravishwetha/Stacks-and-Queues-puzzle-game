2ad476ebaf2098eb0d07e1b8b537c98abab82691
ravishwetha <shwetha.ravi3@gmail.com>
2016-06-17 12:38:49 +0800

    Global path coordinates and movement index

--- Stacks and Queues/Globals.hpp
+++ Stacks and Queues/Globals.hpp
@@ -8,6 +8,7 @@
 
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -15,4 +16,22 @@ extern sf::RenderWindow* window;
 extern float RES_X;
 extern float RES_Y;
 extern float RES_Y2;
+
+/*pathCoords info:
+ Applies to the vector index of the following:
+ (top left x-coordinate) mod 5 = 0
+ (top left y-coordinate) mod 5 = 1
+ (bottom right x-coordinate) mod 5 = 2
+ (bottom right y-coordinate) mod 5 = 3
+ (allowed direction) mod 5 = 4
+ */
+extern std::vector<float> pathCoords;
+
+/*
+ Universal movement index:
+ */
+extern float leftF; //1.0
+extern float upF; //2.0
+extern float downF; //3.0
+
 #endif /* Globals_hpp */
